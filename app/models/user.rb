47a0f2f68a89c19d19fa54855770a74655984d3f
Enrico Favaro <enricofavaro@yahoo.it>
2020-12-05 10:05:40 +0100

    implement sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :dishes
+  has_one_attached :photo
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :email, presence: true
 end
